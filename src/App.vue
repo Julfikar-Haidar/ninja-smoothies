be42de1261517feb3e5cfd7582a57841d86ed2f7
julfikar <julfikar6262@gmail.com>
2020-01-30 12:06:23 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <router-view/>
+   <Index/>
+  </div>
+</template>
+
+<script>
+import Index from './components/Index'
+import Navbar from './components/Navbar'
+
+export default {
+  name: 'app',
+  components: {
+    Index,
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="navbar">
+        <nav class="nav-extended indigo darken-2">
+            <div class="nav-content">
+                <router-link to="">
+                    <span class="nav-title">Ninja Smoothies</span>
+                </router-link>
+                <router-link to="">
+                    <a href="" class="btn-floating btn halfway-fab pink">
+
+                        <i class="material-icons">add</i>
+                    </a>
+                </router-link>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Navbar",
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style >
+.navbar nav{
+    padding:0 10px ;
+}
+</style>
